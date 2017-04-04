c14a55abe7bb84f5bc3e498ca5ff1e5598219d56
freeroid <mkj@mkj.co.in>
2017-04-04 18:08:30 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+  $config_baseHREF = "http://india.dj/";
+
+  $config_databaseServer = "localhost";
+
+  $config_databaseUsername = "";
+
+  $config_databasePassword = "";
+
+  $config_databaseName = "";
+
+  $config_databaseTablePrefix = "";
+
+  $config_adminPassword = "admin"; // pro version only
+?>
